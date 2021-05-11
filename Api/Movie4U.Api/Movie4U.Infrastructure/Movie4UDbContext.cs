25f2fcb37a86f028589b7c9128ca3deccd70ac5e
Chacon25 <32781041+Chacon25@users.noreply.github.com>
2021-05-11 15:53:12 -0600

    Configuration
    Add configuration of Entities

---
+++ Api/Movie4U.Api/Movie4U.Infrastructure/Movie4UDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie4U.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie4U.Infrastructure
+{
+    public class Movie4UDbContext : DbContext
+    {
+        public Movie4UDbContext(DbContextOptions options ) : base(options)
+        {
+                
+        }
+
+
+        public DbSet<Choice> Choice { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ChoiceConfiguration());
+            modelBuilder.ApplyConfiguration(new GenreConfiguration());
+            modelBuilder.ApplyConfiguration(new MovieConfiguration());
+            modelBuilder.ApplyConfiguration(new MovieConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
